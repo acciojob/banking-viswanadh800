efa6c4f03dc627d185c1d69e7bbb7756bce99597
viswanadh800 <viswanadh.800@gmail.com>
2024-02-09 20:27:18 +0530

    Fourth commit

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -36,49 +36,69 @@ public class BankAccount {
         this.minBalance=minBalance;
     }
 
-    private String generateAccountNumberHelper(int digits, int sum){
-        if(digits==0 || 9*digits<sum)
-            return null;
-        if(digits==1)
-            return sum+"";
-        int number[]=new int[digits];
-        sum-=1;
-        for(int i=digits-1;i>=0;i--){
-            if(sum>=9){
-                number[i]=9;
-                sum-=9;
-            }
-            else{
-                number[i]=sum;
-                sum=0;
-            }
-        }
-        number[0]++;
-        StringBuilder sb=new StringBuilder();
-        for(int i=0;i<digits;i++)
-            sb.append(number[i]);
-        return sb.toString();
-    }
+//    private String generateAccountNumberHelper(int digits, int sum){
+//        if(digits==0 || 9*digits<sum)
+//            return null;
+//        if(digits==1)
+//            return sum+"";
+//        int number[]=new int[digits];
+//        sum-=1;
+//        for(int i=digits-1;i>=0;i--){
+//            if(sum>=9){
+//                number[i]=9;
+//                sum-=9;
+//            }
+//            else{
+//                number[i]=sum;
+//                sum=0;
+//            }
+//        }
+//        number[0]++;
+//        StringBuilder sb=new StringBuilder();
+//        for(int i=0;i<digits;i++)
+//            sb.append(number[i]);
+//        return sb.toString();
+//    }
     public String generateAccountNumber(int digits, int sum) throws Exception{
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
-        String number=generateAccountNumberHelper(digits,sum);
-        if(number==null)
+//        String number=generateAccountNumberHelper(digits,sum);
+//        if(number==null)
+//            throw new Exception("Account Number can not be generated");
+//        return number;
+        StringBuilder sb=new StringBuilder();
+        if(9*digits<sum)
             throw new Exception("Account Number can not be generated");
-        return number;
+        else{
+            int numberofnines=sum/9;
+            while(numberofnines!=0){
+                sb.append(9);
+                numberofnines--;
+                digits--;
+            }
+            int rem=sum%9;
+            sb.append(rem);
+            digits--;
+            while(digits>0){
+                sb.append(0);
+                digits--;
+            }
+        }
+        return sb.toString();
     }
 
     public void deposit(double amount) {
         //add amount to balance
-        balance+=amount;
+        this.balance+=amount;
     }
 
     public void withdraw(double amount) throws Exception {
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
-        if(balance-amount<minBalance)
+        if(this.balance-amount<this.minBalance)
             throw new Exception("Insufficient Balance");
-        balance-=amount;
+        else
+            this.balance-=amount;
     }
 
 }
